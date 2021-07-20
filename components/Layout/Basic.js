3252745b697d5fd18a2b1743a318fa9baf1d3a74
Danny <dann_1990@hotmail.com>
2021-07-20 13:36:38 -0400

    START ALL OVER

--- components/Layout/Basic.js
+++ components/Layout/Basic.js
@@ -1,7 +1,6 @@
 import Flex from 'components/Flex/Flex';
 import Header from 'components/Header/Header';
 import Footer from 'components/Footer/Footer';
-import Banner from 'components/Banner/Banner';
 
 import styles from './Basic.module.css';
 
@@ -10,12 +9,12 @@ const Basic = ({ children }) => {
         <meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no" />
         <Flex
             className={styles.mainLayout}
-            justifyContent="center">
+            justifyContent="center"
+            alignItems="center">
             <Flex
                 className={styles.mainWrapper}
                 flexDirection="column">
                 <Header />
-                <Banner />
                 <Flex
                     className={styles.content}
                     flexDirection="column">
